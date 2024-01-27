2a7365e59bc9aff120ca28b06abf64ff12e38048
lucasmercau <lucas.mercau99@gmail.com>
2024-01-27 16:40:27 -0300

    body-parser update

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 
 const mongodb = require("./data/database");
+const bodyParser = require("body-parser");
 
 
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 app.use("/", require("./routes"));
 
 
